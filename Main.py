3c949f78b3f471c12b5257b75f6182b8fc084bed
Anonymoustank <pkadekodi@gmail.com>
2020-07-14 17:34:07 -0600

    Added death screen

--- Main.py
+++ Main.py
@@ -10,32 +10,44 @@ window = pyglet.window.Window(240, 360, "Game", resizable = False)
 window.set_mouse_visible(False)
 space = pymunk.Space()
 space.gravity = 0, -1000
-
+dead = False
+last_click_time = time.perf_counter()
 for i in Moving_Objects.space_list:
     space.add(i)
-
-last_click_time = time.perf_counter()
 
 @window.event
 def on_draw():
     window.clear()
-    x, y = Moving_Objects.body.position
-    Moving_Objects.rocket.position = x - 25, y - 10
-    Moving_Objects.rocket.draw()
-    x, y = Moving_Objects.lower_body.position
-    Moving_Objects.lower_brick.position = x - 14, y - 100
-    Moving_Objects.lower_brick.draw()
-    x, y = Moving_Objects.upper_body.position
-    Moving_Objects.upper_brick.position = x - 14, y - 100
-    Moving_Objects.upper_brick.draw()
+    if dead == False:
+        x, y = Moving_Objects.body.position
+        Moving_Objects.rocket.position = x - 25, y - 10
+        Moving_Objects.rocket.draw()
+        x, y = Moving_Objects.lower_body.position
+        Moving_Objects.lower_brick.position = x - 14, y - 100
+        Moving_Objects.lower_brick.draw()
+        x, y = Moving_Objects.upper_body.position
+        Moving_Objects.upper_brick.position = x - 14, y - 100
+        Moving_Objects.upper_brick.draw()
+    else:
+        label = pyglet.text.Label('Game Over', font_name='Times New Roman', font_size=24, x=window.width//2, y=window.height//2, anchor_x='center', anchor_y='center')
+        label.draw()
 
 def refresh(time):
+    global dead
     space.step(time)
+    rocket_x, rocket_y = Moving_Objects.rocket.position
+    if rocket_y <= -50:
+        dead = True
+    for i in Moving_Objects.wall_list:
+        if len(Moving_Objects.player.shapes_collide(i).points) > 0 or len(Moving_Objects.upper_player.shapes_collide(i).points) > 0:
+            dead = True
     x, y = Moving_Objects.lower_body.position
     Moving_Objects.lower_body.position = x - 2, y
     if x < -15:
         Moving_Objects.lower_body.position = 260, y
     x, y = Moving_Objects.upper_body.position
+    if rocket_y > 400 and x == rocket_x:
+        dead = True
     Moving_Objects.upper_body.position = x - 2, y
     if x < -15:
         Moving_Objects.upper_body.position = 260, y
@@ -45,9 +57,8 @@ def on_mouse_press(x, y, button, modifiers):
     global last_click_time
     if abs(last_click_time - time.perf_counter()) >= 0.3:
         x, y = Moving_Objects.body.position
-        if y <= 210:
-            Moving_Objects.body.apply_force_at_local_point((0, 50000 - y), (0, 50000 - y))
-            last_click_time = time.perf_counter()
+        Moving_Objects.body.apply_force_at_local_point((0, 50000 - y), (0, 50000 - y))
+        last_click_time = time.perf_counter()
 
 if __name__ == "__main__":
     pyglet.clock.schedule_interval(refresh, 1.0/120.0)
--- Moving_Objects.py
+++ Moving_Objects.py
@@ -2,6 +2,7 @@ import pymunk
 import pyglet
 
 space_list = []
+wall_list = []
 
 body = pymunk.Body(1, 100)
 player = pymunk.Poly.create_box(body, size = (25, 20))
@@ -15,7 +16,6 @@ def zero_gravity(body, gravity, damping, dt):
     pymunk.Body.update_velocity(body, (0, -1000), damp_level, dt)
 body.velocity_func = zero_gravity
 
-
 lower_body = pymunk.Body(1, 100, pymunk.Body.KINEMATIC)
 lower_body.position = 240, 0
 lower_pipe = pymunk.Poly.create_box(lower_body, size=(30, 200))
@@ -24,16 +24,17 @@ upper_body = pymunk.Body(1, 100, pymunk.Body.KINEMATIC)
 upper_body.position = 240, 360
 upper_pipe = pymunk.Poly.create_box(upper_body, size=(30, 200))
 
-rocket = pyglet.resource.image("Rocket.png")
+rocket = pyglet.resource.image("Images/Rocket.png")
 rocket.width, rocket.height = 50, 50
 rocket = pyglet.sprite.Sprite(rocket)
 
-lower_brick = pyglet.resource.image("Brick.PNG")
+lower_brick = pyglet.resource.image("Images/Brick.PNG")
 lower_brick.width, lower_brick.height = 30, 200
 lower_brick = pyglet.sprite.Sprite(lower_brick)
 
-upper_brick = pyglet.resource.image("Brick.PNG")
+upper_brick = pyglet.resource.image("Images/Brick.PNG")
 upper_brick.width, upper_brick.height = 30, 200
 upper_brick = pyglet.sprite.Sprite(upper_brick)
 
 space_list = [body, player, upper_player, lower_body, lower_pipe, upper_body, upper_pipe]
+wall_list = [lower_pipe, upper_pipe]
